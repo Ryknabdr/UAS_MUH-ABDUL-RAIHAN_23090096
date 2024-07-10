fc0f66013afb9cf4fc3f79edc1a3b863e632d5f7
bangrehan <149660078+Ryknabdr@users.noreply.github.com>
2024-07-10 12:31:37 +0700

    main.py

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
-from queue import Queue
+from soal3Modul import warmindoer
 
 def main():
-    antrian_pesanan = Queue()
-    menu = ("\nMenu Antrian Pesanan:\n"
-            "1. Tambah Pesanan\n"
-            "2. Hapus Pesanan\n"
-            "3. Tampilkan Antrian\n"
-            "4. Tampilkan Jumlah Pesanan\n"
-            "5. Keluar\n")
-    
+    Soal_3Modul =warmindoer()
+
     while True:
-        print(menu)
-        pilihan = input("Masukkan pilihan Anda (1-5): ")
-        
-        if pilihan == "1":
-            antrian_pesanan(input("Masukkan pesanan yang ingin ditambahkan: "))
-        elif pilihan == "2":
-            dihapus = antrian_pesanan()
-            if dihapus:
-                print(f"Pesanan '{dihapus}' telah dihapus dari antrian.")
-        elif pilihan == "3":
-            antrian_pesanan()
-        elif pilihan == "4":
-            print(f"Jumlah pesanan dalam antrian: {antrian_pesanan()}")
-        elif pilihan == "5":
-            print("Keluar dari program.")
+        print("\nwarmindoer Menu:")
+        print("1. Tambahkan pesanan (enqueue)")
+        print("2. Sajikan pesanan (dequeue)")
+        print("3. Menampilkan queue")
+        print("4. Keluar")
+
+        choice = input("Pilih satu opsi: ")
+
+        if choice == "1":
+            order = input("Masukan Pesanan: ")
+            Soal_3Modul.enqueue(order)
+        elif choice == "2":
+            Soal_3Modul.dequeue()
+        elif choice == "3":
+            Soal_3Modul.display_queue()
+        elif choice == "4":
+            print("Terima Kasih :<.")
             break
         else:
-            print("Pilihan tidak valid. Silakan masukkan pilihan yang benar.")
+            print("Opsi tidak valid. Silakan coba lagi.")
 
 if __name__ == "__main__":
     main()
